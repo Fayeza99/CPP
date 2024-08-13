642211d7b0b1834668d775829f15a691ebfd46e7
Fayeza99 <fayezanikzad@yahoo.com>
2024-08-13 13:27:08 +0200

    ex02 added

---
+++ ex02/Funcs.cpp
@@ -0,0 +1,51 @@
+#include "Base.hpp"
+
+Base* generate()
+{
+	std::srand(std::time(NULL));
+	int r = std::rand() % 3;
+	switch(r) {
+		case 0:
+			return new A();
+		case 1:
+			return new B();
+		case 2:
+			return new C();
+		default:
+			return NULL;
+	}
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p)) {
+		std::cout << "A" << std::endl;
+	} else if (dynamic_cast<B*>(p)) {
+		std::cout << "B" << std::endl;
+	} else if (dynamic_cast<C*>(p)) {
+		std::cout << "C" << std::endl;
+	} else {
+		std::cout << "Unknown" << std::endl;
+	}
+}
+
+void identify(Base& p)
+{
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::bad_cast&) {
+		try {
+			(void)dynamic_cast<B&>(p);
+			std::cout << "B" << std::endl;
+		} catch (std::bad_cast&) {
+			try{
+				(void)dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			} catch (std::bad_cast&) {
+				std::cout << "Unknown" << std::endl;
+			}
+		}
+	}
+}
+
